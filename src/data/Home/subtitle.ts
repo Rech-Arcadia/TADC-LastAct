4d65d4de6422960f5748ed6825db0db4e0c49082
Rech-Arcadia <arcadia.avalos.ricardo@gmail.com>
2026-07-20 23:58:35 -0600

    feat: add subtitle data for the Home component

---
+++ src/data/Home/subtitle.ts
@@ -0,0 +1,8 @@
+export const subtitle = {
+  subtitle: 'El último acto comienza',
+  title: 'Algunas historias terminan, otras comienzan',
+  phrase:
+    'Cuando las luces del circo se apaguen, solo quedará una elección: despertar… o seguir actuando',
+  ctaPrimary: 'Descubre la historia',
+  ctaSecondary: 'Ver tráiler',
+}
