e5be9d37988a4f29adefea23bdf2cfe3e41546ca
Rech-Arcadia <arcadia.avalos.ricardo@gmail.com>
2026-07-21 00:05:36 -0600

    fix: remove clamp from hero title font size for consistent styling

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -78,7 +78,7 @@ onUnmounted(() => {
     >
       <div class="mx-auto flex h-full w-full items-center justify-center">
         <h1
-          class="text-center text-[clamp(3rem,10.5vw,200px)] font-semibold uppercase leading-none text-white [text-shadow:0_0_2px_rgba(255,255,255,1),0_0_4px_rgba(255,255,255,.8),0_0_16px_rgba(255,255,255,.55)]"
+          class="text-center uppercase leading-none text-white [text-shadow:0_0_2px_rgba(255,255,255,1),0_0_4px_rgba(255,255,255,.8),0_0_16px_rgba(255,255,255,.55)]"
           id="hero-title"
         >
           <span class="block">The Last</span>
