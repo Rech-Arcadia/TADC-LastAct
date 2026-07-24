96d9403336133d7f6d14f633d0d963d697d557f2
Rech-Arcadia <arcadia.avalos.ricardo@gmail.com>
2026-07-24 00:59:40 -0600

    feat: add JaxVoid component to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Hero from '@/components/Home/Hero.vue'
+import JaxVoid from '@/views/Jax-void.vue'
 </script>
 
 <template>
   <Hero />
+  <JaxVoid />
 </template>
 
 <style scoped></style>
