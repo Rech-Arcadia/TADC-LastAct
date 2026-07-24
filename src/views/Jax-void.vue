660e18a8b8fb172edcee0a8e5bb0f53538952a97
Rech-Arcadia <arcadia.avalos.ricardo@gmail.com>
2026-07-24 00:59:48 -0600

    feat: create Jax-Void component with basic layout

---
+++ src/views/Jax-void.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="relative min-h-dvh w-full overflow-hidden" id="hero">
+    <div
+      class="flex h-screen w-screen items-center justify-center bg-gray-950 text-4xl font-bold text-gray-50"
+    >
+      Jax-Void
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
