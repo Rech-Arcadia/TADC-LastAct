9cd784d63693774a69bbaecdb03ab6f143f598b8
Rech-Arcadia <arcadia.avalos.ricardo@gmail.com>
2026-07-24 00:59:20 -0600

    feat: add JaxVoid route to the router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
+import JaxVoid from '@/views/Jax-void.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/jax-void',
+      name: 'jax-void',
+      component: JaxVoid,
     },
   ],
 })
