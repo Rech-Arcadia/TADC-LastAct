c1e38b7e8f74d367f77391524f61afb981c903ad
Rech-Arcadia <arcadia.avalos.ricardo@gmail.com>
2026-07-25 01:05:12 -0600

    feat: add DotsNav component to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Hero from '@/components/Home/Hero.vue'
 import JaxVoid from '@/views/Jax-void.vue'
+import DotsNav from '@/components/common/dots-nav.vue'
 </script>
 
 <template>
   <Hero />
   <JaxVoid />
+  <DotsNav />
 </template>
 
 <style scoped></style>
