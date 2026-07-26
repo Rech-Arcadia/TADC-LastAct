ff5a109e3ad1df000f57295dd240b029f0ca0e07
Rech-Arcadia <arcadia.avalos.ricardo@gmail.com>
2026-07-26 02:59:19 -0600

    feat: add InfoCharacters component to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Hero from '@/components/Home/Hero.vue'
 import JaxVoid from '@/views/Jax-void.vue'
+import InfoCharacters from '@/views/InfoCharacters.vue'
 import DotsNav from '@/components/common/dots-nav.vue'
 </script>
 
@@ -8,6 +9,7 @@ import DotsNav from '@/components/common/dots-nav.vue'
   <Hero />
   <JaxVoid />
   <DotsNav />
+  <InfoCharacters />
 </template>
 
 <style scoped></style>
